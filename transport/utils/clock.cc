chachako/android-studio

commit 17d7a81c157c56dbaaba3e2d6bb24e496a65664a

Minor refactor: removing int64->double->int64 conversion.
Test: manual
Bug: none

Change-Id: I39df76c4dcd3592003256c7bb1ea897174894161

Former-commit-id: 3c47917e46d658f5b3e2fee451c5ec63829c8fc1

transport/utils/clock.cc
@@ -22,7 +22,7 @@ namespace profiler {
 int64_t SteadyClock::GetCurrentTime() const {
   timespec time;
   clock_gettime(CLOCK_MONOTONIC, &time);
-  return 1e9 * time.tv_sec + time.tv_nsec;
+  return 1000000000 * time.tv_sec + time.tv_nsec;
 }
 
 }  // namespace profiler

commit e86b0c38635a8340b522090e17f4faa623c00379

Fix SteadyClock int overflow on 32-bit platforms
Broken by 17d7a81c157c56dbaaba3e2d6bb24e496a65664a [formerly 3c47917e46d658f5b3e2fee451c5ec63829c8fc1]

Bug: N/A
Test: N/A
Change-Id: I6d6697fbafb333d689b8795be020fbba0952b0f2

Former-commit-id: 9b0c3e79ba0b30065bcc15283bb297855ae43963

transport/utils/clock.cc
@@ -22,7 +22,7 @@ namespace profiler {
 int64_t SteadyClock::GetCurrentTime() const {
   timespec time;
   clock_gettime(CLOCK_MONOTONIC, &time);
-  return 1000000000 * time.tv_sec + time.tv_nsec;
+  return static_cast<int64_t>(1000000000) * time.tv_sec + time.tv_nsec;
 }
 
 }  // namespace profiler